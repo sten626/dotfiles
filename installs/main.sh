3b7a9950bf2f97a4e5f3d8cbdea7bbbc79456c85
Steven Indzeoski <steven.indzeoski@gmail.com>
2022-04-14 16:38:20 -0700

    feat: More centos support changes.
    Issue #38

--- installs/main.sh
+++ installs/main.sh
@@ -3,3 +3,14 @@
 cd "$(dirname "${BASH_SOURCE[0]}")" && . ../utils.sh
 
 print_in_cyan "\n • Install packages\n\n"
+
+os="$(get_os)"
+readonly os
+readonly os_installer="./$os/main.sh"
+
+# shellcheck disable=SC1090
+if [[ -f $os_installer ]]; then
+  . "$os_installer"
+else
+  print_success "No package installer found for $os."
+fi
