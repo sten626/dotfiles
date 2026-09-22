4ab819ee00fbef5571ad65391a3d5ab14d5178a7
Steven Indzeoski <steven.indzeoski@gmail.com>
2026-09-22 14:58:46 -0600

    Fixing bad XDG location

--- dot_config/bash/env.sh
+++ dot_config/bash/env.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# XDG Base Directory Specification
-export XDG_CONFIG_HOME=$HOME/.config
-export XDG_CACHE_HOME=$HOME/.cache
-export XDG_DATA_HOME=$HOME/.local/share
-
 path_remove() {
   local -r path_to_remove="$1"
   [ -n "$path_to_remove" ] || return 0
